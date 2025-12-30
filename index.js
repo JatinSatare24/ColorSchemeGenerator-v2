14d8e370177ab2448aa8353a0d460e57b9db7b7b
Jatin Satare <jatinsatare@gmail.com>
2025-12-30 16:16:56 +0530

    Added logic

---
+++ index.js
@@ -0,0 +1,22 @@
+const selectedColor = document.getElementById("input-color")
+const selectedColorScheme = document.getElementById("scheme-select")
+const btn = document.getElementById("btn")
+const colorContainer = document.getElementById("colors")
+const colorHex = document.getElementById("color-hex")
+
+
+btn.addEventListener("click", function () {
+    fetch(`https://www.thecolorapi.com/scheme?hex=${selectedColor.value.substring(1)}&mode=${selectedColorScheme.value}`)
+        .then(res => res.json())
+        .then(
+            data => {
+                colorContainer.innerHTML = ''
+                colorHex.innerHTML = ''
+                data.colors.forEach(color => {
+                    colorContainer.innerHTML += `<div id="singleColorDiv" style="background-color: ${color.hex.value};"><div>`
+                    colorHex.innerHTML += `<div id="hexValues"> ${color.hex.value}</div>`
+                });
+            }
+
+        )
+})
